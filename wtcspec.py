3a42121e1c054181323cdfdbbe61d5adc921fd9c
boonleng <boonleng@ou.edu>
2021-03-07 10:33:30 -0600

    Added wtcspec.py

--- wtcspec.py
+++ wtcspec.py
@@ -31,17 +31,6 @@ path_effects = [
 
 ###
 
-def proc_file(filename, verbose=0):
-    s = time.time()
-    ray_pulses, cpi_headers = toshi.read(filename)
-    e = time.time()
-    if verbose:
-        print('{} read in {:.2f} s'.format(os.path.basename(filename), e - s))
-    
-    return ray_pulses, cpi_headers
-
-###
-
 # Waveform table
 waveform = toshi.tx_waveform()
 pc_nfft = 1024
@@ -55,9 +44,13 @@ turbines = np.array([[pt[2], pt[1]] for pt in wtc_loc], dtype=np.single)
 data_home = os.path.expanduser('~/Downloads/toshiba/AKITA_IQ_TO_OU/IQdata')
 files = glob.glob('{}/2021.01.15/*/*.dat'.format(data_home), recursive=True)
 files = sorted(files)
-filename = files[0]
+filename = files[1]
 
-ray_pulses, cpi_headers = proc_file(filename, verbose=1)
+# Read the raw data
+tic = time.time()
+ray_pulses, cpi_headers = toshi.read(filename)
+toc = time.time()
+print('{} read in {:.2f} s'.format(os.path.basename(filename), toc - tic))
 
 # Go through the pulses for azimuth
 az = np.zeros(len(ray_pulses), dtype=np.single)
@@ -66,10 +59,9 @@ for k, pulses in enumerate(ray_pulses):
 
 # Dimensions
 naz = len(ray_pulses)
-ngate_long = ray_pulses[0][0].cpi_header.num_range_long_hi
 ngate_short_hi = ray_pulses[0][0].cpi_header.num_range_short_hi
 ngate_short_lo = ray_pulses[0][0].cpi_header.num_range_short_lo
-ngate = ngate_long + ngate_short_hi + ngate_short_lo
+ngate_long = ray_pulses[0][0].cpi_header.num_range_long_hi
 
 scan_time = time.strptime(os.path.basename(filename)[:15], '%Y%m%d_%H%M%S')
 scan_el = ray_pulses[0][0].elevation
@@ -77,12 +69,13 @@ scan_el = ray_pulses[0][0].elevation
 # Sampling code from the CPI header
 fs = 1.0e6 * (1 << cpi_headers[0].fs_code)
 dr = 3.0e8 / fs / 2
-r = 1.0e-3 * (np.arange(0, ngate, dtype=np.single) * dr + 0.5 * dr)
+r = 1.0e-3 * (np.arange(0, ngate_long, dtype=np.single) * dr + 0.5 * dr)
 
 # Cell indices of the cells with wind turbines
 cid = cspec.pos2cellid(turbines, np.unique(az), r)
 
-use_window = True
+# Revise ngate since I only care up to this gate, pad a bit extra
+ngate = np.max(np.array(cid)[:, 1]) + 10
 
 # Go through the pulses
 cpuls = []
@@ -100,19 +93,57 @@ for k, (pulses, cpi_header) in enumerate(zip(ray_pulses, cpi_headers)):
     p = np.zeros((npulse, ngate), dtype=np.csingle)
     for j, pulse in enumerate(pulses):
         c = np.exp(-1j * pulse.phase_h_short)                              # Phase code
-        p[j, :ngate_long] = pc[j, :ngate_long]                             # Long only
+        p[j, :ngate] = pc[j, :ngate]                                       # Long only
         p[j, :ngate_short_hi] = pulse.h_short_hi * c                       # Short hi
         p[j, :ngate_short_lo] = pulse.h_short_lo * c                       # Short lo        
 
-    cpuls.append(pc[:ngate])                                                       # Keep a copy of the compressed pulse
+    cpuls.append(p)                                                        # Keep a copy of the compressed pulse
 
-    # Data windowing
-    if use_window:
-        w = scipy.signal.get_window('blackmanharris', len(p))
-    else:
-        w = np.ones((p.shape[0],))
-    w /= np.sqrt(np.sum(w ** 2)) / np.sqrt(p.shape[0])                     # Normalize to non-windowed gain
-    ww = np.repeat(np.expand_dims(w, axis=1), ngate, axis=1)               # Make same shape
-    p *= ww                                                                # Windowing
+# BScope - only look at even rays since it was operated in dual PRF mode
+# Unable to determine PRT, best guess is slightly > ngate_long x fs = 0.396 ms
+# Assume 0.4 ms, which corresponds to 2500 Hz PRF, reasonable
+bscope = np.zeros((ngate, int(naz / 2)), dtype=np.single)
+for k, p in enumerate(cpuls[::2]):
+    bscope[:, k] = np.mean(np.abs(p / 32e3) ** 2, axis=0)
+bscope = 10.0 * np.log10(bscope)
+m = [x[1] for x in cid]
+n = cpuls[0].shape[0]
+te = np.arange(int(naz / 2) + 1) * n * 0.4e-3
+re = 1.0e-3 * np.arange(ngate + 1) * dr
 
-for cpul in
+plt.pcolormesh(te, re, bscope)
+plt.plot(np.ones((len(m),)), r[m], 'xw', linewidth=0.5)
+plt.clim((-60, 0))
+plt.colorbar()
+plt.xlabel('Time (s)')
+plt.ylabel('Range (km)')
+plt.title('Relative Signal Strength (dB)')
+
+def spectrogram(igate):
+    nfft = 1024
+    n = cpuls[0].shape[0]
+    tscope = np.zeros((int(naz/2), nfft), dtype=np.csingle)
+    w = scipy.signal.get_window('blackmanharris', n)
+    w /= np.sqrt(np.mean(w ** 2))
+    for k, cpul in enumerate(cpuls[::2]):
+        p = cpul[:, igate] / 32000
+        tscope[k, :] = sp.fft.fft(p * w, nfft, axis=0)
+    tscope = sp.fft.fftshift(tscope, axes=(1,))
+    tscope = 10.0 * np.log10(np.abs(tscope) ** 2 / np.sqrt(nfft))
+    
+    te = np.arange(naz + 1) * n * 0.4e-3
+    omega = np.arange(-nfft / 2, nfft / 2 + 1) / nfft
+    plt.figure()
+    plt.pcolormesh(omega, te[::2], tscope)
+    plt.clim((-60, -10))
+    cb = plt.colorbar()
+    cb.ax.set_ylabel('Relative Signal Strength (dB)')
+    plt.xlabel('Discrete Frequency (x $\pi$) (rad/sample)')
+    plt.ylabel('Time (s)')
+    plt.title('Spectogram of Cell #{} ({:.2f} m)'.format(igate, r[igate]))
+
+spectrogram(107)
+plt.ylim((0, 5))
+
+# for igate in [107, 113, 117]:
+#     spectrogram(igate)
