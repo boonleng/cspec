c86265fa0cf62a6e229a9b34eb1fb85e7ba98404
boonleng <boonleng@ou.edu>
2021-03-08 10:19:34 -0600

    Made notebooks more consistent

--- wtcspec.py
+++ wtcspec.py
@@ -44,7 +44,7 @@ turbines = np.array([[pt[2], pt[1]] for pt in wtc_loc], dtype=np.single)
 data_home = os.path.expanduser('~/Downloads/toshiba/AKITA_IQ_TO_OU/IQdata')
 files = glob.glob('{}/2021.01.15/*/*.dat'.format(data_home), recursive=True)
 files = sorted(files)
-filename = files[1]
+filename = files[4]
 
 # Read the raw data
 tic = time.time()
@@ -74,9 +74,6 @@ r = 1.0e-3 * (np.arange(0, ngate_long, dtype=np.single) * dr + 0.5 * dr)
 # Cell indices of the cells with wind turbines
 cid = cspec.pos2cellid(turbines, np.unique(az), r)
 
-# Revise ngate since I only care up to this gate, pad a bit extra
-ngate = np.max(np.array(cid)[:, 1]) + 10
-
 # Go through the pulses
 cpuls = []
 for k, (pulses, cpi_header) in enumerate(zip(ray_pulses, cpi_headers)):
@@ -90,27 +87,36 @@ for k, (pulses, cpi_header) in enumerate(zip(ray_pulses, cpi_headers)):
     pc = sp.fft.ifft(pf * wf, n=pc_nfft, axis=1)                           # Return to time domain
 
     # Gather the short pulses and the compressed long pulses
-    p = np.zeros((npulse, ngate), dtype=np.csingle)
+    p = np.zeros((npulse, ngate_long), dtype=np.csingle)
     for j, pulse in enumerate(pulses):
         c = np.exp(-1j * pulse.phase_h_short)                              # Phase code
-        p[j, :ngate] = pc[j, :ngate]                                       # Long only
+        p[j, :ngate_long] = pc[j, :ngate_long]                             # Long only
         p[j, :ngate_short_hi] = pulse.h_short_hi * c                       # Short hi
         p[j, :ngate_short_lo] = pulse.h_short_lo * c                       # Short lo        
 
     cpuls.append(p)                                                        # Keep a copy of the compressed pulse
 
+# Revise ngate since I only care up to this gate, pad a bit extra
+ngate = np.max(np.array(cid)[:, 1]) + 10
+
 # BScope - only look at even rays since it was operated in dual PRF mode
 # Unable to determine PRT, best guess is slightly > ngate_long x fs = 0.396 ms
 # Assume 0.4 ms, which corresponds to 2500 Hz PRF, reasonable
+prt = 0.4e-3
 bscope = np.zeros((ngate, int(naz / 2)), dtype=np.single)
 for k, p in enumerate(cpuls[::2]):
-    bscope[:, k] = np.mean(np.abs(p / 32e3) ** 2, axis=0)
+    bscope[:, k] = np.mean(np.abs(p[:, :ngate] / 32e3) ** 2, axis=0)
 bscope = 10.0 * np.log10(bscope)
 m = [x[1] for x in cid]
 n = cpuls[0].shape[0]
-te = np.arange(int(naz / 2) + 1) * n * 0.4e-3
+te = np.arange(int(naz / 2) + 1) * n * prt
 re = 1.0e-3 * np.arange(ngate + 1) * dr
+basename = os.path.basename(filename)
+timestr = time.strftime('%Y%m%d-%H%M%S', scan_time)
+folder = os.path.expanduser('~/Downloads/toshiba/figs')
 
+# BScope
+fig = plt.figure()
 plt.pcolormesh(te, re, bscope)
 plt.plot(np.ones((len(m),)), r[m], 'xw', linewidth=0.5)
 plt.clim((-60, 0))
@@ -118,32 +124,38 @@ plt.colorbar()
 plt.xlabel('Time (s)')
 plt.ylabel('Range (km)')
 plt.title('Relative Signal Strength (dB)')
+plt.text(0.98 * te[-1], 0.95 * re[-1], basename, ha='right',
+         fontsize=9, fontweight='bold', color='white', path_effects=path_effects)
+fig.savefig('{}/bscope-{}.png'.format(folder, timestr))
 
 def spectrogram(igate):
     nfft = 1024
     n = cpuls[0].shape[0]
-    tscope = np.zeros((int(naz/2), nfft), dtype=np.csingle)
+    stride = slice(1, None, 2)
+    tscope = np.zeros((len(az[stride]), nfft), dtype=np.csingle)
     w = scipy.signal.get_window('blackmanharris', n)
     w /= np.sqrt(np.mean(w ** 2))
-    for k, cpul in enumerate(cpuls[::2]):
+    for k, cpul in enumerate(cpuls[stride]):
         p = cpul[:, igate] / 32000
         tscope[k, :] = sp.fft.fft(p * w, nfft, axis=0)
     tscope = sp.fft.fftshift(tscope, axes=(1,))
     tscope = 10.0 * np.log10(np.abs(tscope) ** 2 / np.sqrt(nfft))
-    
-    te = np.arange(naz + 1) * n * 0.4e-3
+    # Axes for plot
+    te = np.arange(len(az[stride]) + 1) * stride.step * n * prt
     omega = np.arange(-nfft / 2, nfft / 2 + 1) / nfft
-    plt.figure()
-    plt.pcolormesh(omega, te[::2], tscope)
+    # Figure
+    fig = plt.figure()
+    plt.pcolormesh(omega, te, tscope)
+    plt.text(-0.48, 0.28, basename, ha='left',
+         fontsize=9, fontweight='bold', color='white', path_effects=path_effects)
+    plt.ylim((0, 7))
     plt.clim((-60, -10))
     cb = plt.colorbar()
     cb.ax.set_ylabel('Relative Signal Strength (dB)')
     plt.xlabel('Discrete Frequency (x $\pi$) (rad/sample)')
     plt.ylabel('Time (s)')
-    plt.title('Spectogram of Cell #{} ({:.2f} m)'.format(igate, r[igate]))
+    plt.title('Spectrogram of Cell #{} ({:.2f} km)'.format(igate, r[igate]))
+    fig.savefig('{}/spg-{}-c{:03d}.png'.format(folder, timestr, igate))
 
-spectrogram(107)
-plt.ylim((0, 5))
-
-# for igate in [107, 113, 117]:
-#     spectrogram(igate)
+for igate in m:
+    spectrogram(igate)
